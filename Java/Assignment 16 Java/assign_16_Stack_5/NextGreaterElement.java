fbdc8f1b7405f304e7038cd7c2558d66504d6d55
Shreyash <shreyashdhumale24@gmail.com>
2024-06-02 18:56:06 +0530

    Assignment 16

--- Java/Assignment 16 Java/assign_16_Stack_5/NextGreaterElement.java
+++ Java/Assignment 16 Java/assign_16_Stack_5/NextGreaterElement.java
@@ -1,5 +1,37 @@
 package assign_16_Stack_5;
 
-public class NextGreaterElement {
+import java.util.Stack;
 
+public class NextGreaterElement {
+    public static void main(String[] args) {
+        int[] input1 = {12,13,14,15};
+        
+        int[] result1 = findNextGreaterElement(input1);
+        
+        System.out.println("Next greater elements:");
+        printArray(result1);
+   
+    }
+
+    public static int[] findNextGreaterElement(int[] nums) {
+        int[] result = new int[nums.length];
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int i = nums.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() <= nums[i]) {
+                stack.pop();
+            }
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(nums[i]);
+        }
+        
+        return result;
+    }
+    
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
 }
--- Java/Assignment 16 Java/assign_16_Vector_4/VectorManipulation.java
+++ Java/Assignment 16 Java/assign_16_Vector_4/VectorManipulation.java
@@ -1,5 +1,47 @@
 package assign_16_Vector_4;
 
-public class VectorManipulation {
+import java.util.Vector;
 
+public class VectorManipulation {
+    public static void main(String[] args) {
+        Vector<Integer> vector1 = new Vector<>();
+        Vector<Integer> vector2 = new Vector<>();
+
+        vector1.add(2);
+        vector1.add(3);
+        vector1.add(4);
+
+        vector2.add(5);
+        vector2.add(6);
+        vector2.add(7);
+
+        Vector<Integer> addition = vectorAddition(vector1, vector2);
+        Vector<Integer> subtraction = vectorSubtraction(vector1, vector2);
+
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+    }
+
+    public static Vector<Integer> vectorAddition(Vector<Integer> vector1, Vector<Integer> vector2) {
+        if (vector1.size() != vector2.size()) {
+            throw new IllegalArgumentException();
+        }
+        Vector<Integer> result = new Vector<>();
+        for (int i = 0; i < vector1.size(); i++) {
+            result.add(vector1.get(i) + vector2.get(i));
+        }
+        return result;
+    }
+
+    public static Vector<Integer> vectorSubtraction(Vector<Integer> vector1, Vector<Integer> vector2) {
+        if (vector1.size() != vector2.size()) {
+            throw new IllegalArgumentException();
+        }
+        Vector<Integer> result = new Vector<>();
+        for (int i = 0; i < vector1.size(); i++) {
+            result.add(vector1.get(i) - vector2.get(i));
+        }
+        return result;
+    }
 }
+
