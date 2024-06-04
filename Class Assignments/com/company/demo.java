8b0edec9695cfcc997ac2be706be89743fcf18a4
Shreyash <shreyashdhumale24@gmail.com>
2024-06-04 22:29:43 +0530

    class Assignment

---
+++ Class Assignments/com/company/demo.java
@@ -0,0 +1,18 @@
+public class demo
+    {
+        void m1()
+        {
+            System.out.println("m1 method called");
+        }
+    }
+    class MainClass
+    {
+        public static void main(String[] args)
+        {
+            demo d1=new demo();
+            d1.m1();
+        }
+    }
+
+
+
