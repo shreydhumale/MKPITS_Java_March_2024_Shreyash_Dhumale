f74117fe4d341f52ea9d983a07732cac8f6d8524
Shreyash <shreyashdhumale24@gmail.com>
2024-06-07 10:30:33 +0530

    Assignment 20

---
+++ Java/Assignment 20 Java/Assign_20_20.java
@@ -0,0 +1,18 @@
+package Assignment_20_Java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Assign_20_20 {
+	    public static void main(String[] args) {
+	        List<List<Integer>> list=new ArrayList<>();
+	        list.add(Arrays.asList(10,20,30));
+	        list.add(Arrays.asList(40,50,60));
+	        list.add(Arrays.asList(70,80,90));
+	        System.out.println("The list before flattening is : "+list);
+	         List a=  list.stream().flatMap(List::stream).collect(Collectors.toList());
+	          System.out.println(a);
+	    }
+	}
