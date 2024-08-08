ffb4ed0135039b56dda417735c93a2bdd49a34d5
Shreyash <shreyashdhumale24@gmail.com>
2024-08-08 11:33:42 +0530

    spring setter injection

---
+++ Spring framework/08-08-2024/Spring_Setter_injection/src/com/example/Student.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import com.example.Address;
+public class Student {
+	private String name;
+	private int marks;
+    private Address address;
+
+    public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+}
